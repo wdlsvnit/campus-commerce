670685d31d06c05966178b863c2c317d96a94a1b
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2018-04-07 14:53:58 +0530

    feat: Add Post model

--- app/src/main/java/in/webdevlabs/campuscommerce/model/Post.kt
+++ app/src/main/java/in/webdevlabs/campuscommerce/model/Post.kt
@@ -3,4 +3,17 @@ package `in`.webdevlabs.campuscommerce.model
 /**
  * Created by yolo on 7/4/18.
  */
-class Post(val name : String)
+data class Post(
+        val pid: String,
+        val name: String,
+        val price: Int,
+        val uid: String,
+        val time: String,
+        val type: PostType,
+        val tags: List<String>
+)
+
+enum class PostType(val type: String) {
+    SELL("sell"),
+    RENT("rent")
+}
