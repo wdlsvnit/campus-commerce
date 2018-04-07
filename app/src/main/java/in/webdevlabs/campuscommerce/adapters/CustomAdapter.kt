2ade30df4c6716326202cb9fe1e05134557b0a31
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2018-04-07 21:37:45 +0530

    fix: Realtime bug

--- app/src/main/java/in/webdevlabs/campuscommerce/adapters/CustomAdapter.kt
+++ app/src/main/java/in/webdevlabs/campuscommerce/adapters/CustomAdapter.kt
@@ -5,8 +5,7 @@ package `in`.webdevlabs.campuscommerce.adapters
  */
 import `in`.webdevlabs.campuscommerce.R
 import `in`.webdevlabs.campuscommerce.model.Post
-import android.content.Context
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView
 import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
@@ -17,39 +16,35 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 
-class CustomAdapter(private val context : Context, private val list : List<Post>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>(){
+class CustomAdapter(val list: ArrayList<Post>) : RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var name: TextView
-        var price: TextView
-        var type: TextView
-        var chat: ImageView
-        var time: TextView
-
-        init {
-            name = itemView.findViewById(R.id.name) as TextView
-            price = itemView.findViewById(R.id.price) as TextView
-            type = itemView.findViewById(R.id.type) as TextView
-            time = itemView.findViewById(R.id.time) as TextView
-            chat = itemView.findViewById(R.id.chat) as ImageView
-
-        }
+        var name: TextView = itemView.findViewById(R.id.name) as TextView
+        var price: TextView = itemView.findViewById(R.id.price) as TextView
+        var type: TextView = itemView.findViewById(R.id.type) as TextView
+        var chat: ImageView = itemView.findViewById(R.id.chat) as ImageView
+        var time: TextView = itemView.findViewById(R.id.time) as TextView
     }
-    override fun onCreateViewHolder(parent : ViewGroup, type : Int) : ViewHolder {
-        val view : View = LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false);
-        return ViewHolder(view);
+
+    override fun onCreateViewHolder(parent: ViewGroup, type: Int): ViewHolder {
+        val view: View = LayoutInflater.from(parent.context).inflate(R.layout.item_post, parent, false);
+        return ViewHolder(view)
     }
-    override fun onBindViewHolder(holder : ViewHolder, position : Int){
-        var post : Post = list.get(position)
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post: Post = list[position]
+
         holder.name.text = post.name
-
-        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.getDefault())
-        holder.time.text = DateUtils.getRelativeTimeSpanString(sdf.parse(post.time).time,System.currentTimeMillis(),DateUtils.MINUTE_IN_MILLIS)
         holder.price.text = post.price.toString()
         holder.type.text = post.type.toString()
-    }
-    override fun getItemCount() : Int{
-        return list.size;
+
+        val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX", Locale.getDefault())
+        post.time?.run {
+            holder.time.text = DateUtils.getRelativeTimeSpanString(sdf.parse(post.time).time, System.currentTimeMillis(), DateUtils.MINUTE_IN_MILLIS)
+        }
     }
 
+    override fun getItemCount(): Int {
+        return list.size
+    }
 }
--- app/src/main/java/in/webdevlabs/campuscommerce/fragments/HomeFragment.kt
+++ app/src/main/java/in/webdevlabs/campuscommerce/fragments/HomeFragment.kt
@@ -12,10 +12,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -24,26 +24,49 @@ class HomeFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val database: FirebaseDatabase = FirebaseDatabase.getInstance()
-        val mPostReference = database.getReference()
+        val mPostReference = database.reference
 
         val list = ArrayList<Post>();
         val rView = view.findViewById(R.id.my_recycler_view) as RecyclerView;
-        val adapter = CustomAdapter(activity!!.baseContext, list)
+        val adapter = CustomAdapter(list)
 
-        mPostReference.child("posts").addListenerForSingleValueEvent(object : ValueEventListener {
-            override fun onDataChange(dataSnapshot: DataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    list.clear()
-                    for (ds in dataSnapshot.children) {
-                        val post = ds.getValue<Post>(Post::class.java)
-                        list.add(Post(post!!.pid, post.name, post.price, post.uid, post.time, post.type, post.tags))
-                    }
+        /* mPostReference.child("posts").addListenerForSingleValueEvent(object : ValueEventListener {
+             override fun onDataChange(dataSnapshot: DataSnapshot) {
+                 if (dataSnapshot.exists()) {
+                     list.clear()
+                     for (ds in dataSnapshot.children) {
+                         val post = ds.getValue<Post>(Post::class.java)
+                         list.add(Post(post!!.pid, post.name, post.price, post.uid, post.time, post.type, post.tags))
+                     }
+                     adapter.notifyDataSetChanged()
+                 }
+             }
+
+             override fun onCancelled(firebaseError: DatabaseError) {
+             }
+         })*/
+
+        mPostReference.child("posts").addChildEventListener(object : ChildEventListener {
+            override fun onCancelled(databaseError: DatabaseError?) {
+            }
+
+            override fun onChildMoved(dataSnapshot: DataSnapshot?, key: String?) {
+            }
+
+            override fun onChildChanged(dataSnapshot: DataSnapshot?, key: String?) {
+            }
+
+            override fun onChildAdded(dataSnapshot: DataSnapshot?, key: String?) {
+                val post = dataSnapshot?.getValue(Post::class.java)
+                if (post != null) {
+                    adapter.list.add(post)
                     adapter.notifyDataSetChanged()
                 }
             }
 
-            override fun onCancelled(firebaseError: DatabaseError) {
+            override fun onChildRemoved(dataSnapshot: DataSnapshot?) {
             }
+
         })
 
         rView.adapter = adapter
