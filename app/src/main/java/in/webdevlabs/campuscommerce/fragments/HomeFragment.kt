8ba55fe9d9b12b344ea3b461e16cff525914876f
Shailesh Baldaniya <shaileshbaldaniya351@gmail.com>
2018-04-07 19:55:31 +0530

    fix: Fix bug

--- app/src/main/java/in/webdevlabs/campuscommerce/fragments/HomeFragment.kt
+++ app/src/main/java/in/webdevlabs/campuscommerce/fragments/HomeFragment.kt
@@ -12,18 +12,11 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.util.Log
-import com.firebase.ui.database.FirebaseListAdapter
-import com.google.firebase.database.*
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.firebase.client.Firebase
-import android.widget.TextView
-
-
-
-
-
-
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -32,31 +25,10 @@ class HomeFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
         val database: FirebaseDatabase = FirebaseDatabase.getInstance()
         val mPostReference = database.getReference()
-//        val ref = Firebase("https://the-benders.firebaseio.com/")
-//        val adapter = object : FirebaseListAdapter<Post>(activity, Post::class.java,R.layout.item_post,ref) {
-//            override fun populateView(v: View?, model: Post?, position: Int) {
-//
-//            }
-//
-//        }
 
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
         val list = ArrayList<Post>();
-        prepareList(list);
         val rView = view.findViewById(R.id.my_recycler_view) as RecyclerView;
         val adapter = CustomAdapter(activity!!.baseContext, list)
-        rView.adapter = adapter;
-        rView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
-        return view
-    }
-    private fun prepareList(list : ArrayList<Post>){
-//        list.add(Post("post1"))
-//        list.add(Post("post2"))
-//        list.add(Post("post2"))
-//        list.add(Post("post2"))
-//        list.add(Post("post2"))
-//        list.add(Post("post2"))
-//        list.add(Post("post2"))
 
         mPostReference.child("posts").addListenerForSingleValueEvent(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -64,17 +36,27 @@ class HomeFragment : Fragment() {
                     list.clear()
                     for (ds in dataSnapshot.children) {
                         val post = ds.getValue<Post>(Post::class.java)
-                        list.add(Post(post!!.pid,post.name,post.price,post.uid,post.time,post.type,post.tags))
+                        list.add(Post(post!!.pid, post.name, post.price, post.uid, post.time, post.type, post.tags))
                     }
                     adapter.notifyDataSetChanged()
                 }
             }
+
             override fun onCancelled(firebaseError: DatabaseError) {
             }
         })
 
         rView.adapter = adapter
-        rView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL,false)
+        rView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        fab.setOnClickListener {
+            val intent = Intent(activity, NewPostActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
